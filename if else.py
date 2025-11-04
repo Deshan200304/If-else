e97aa49e7aea6500a9691df5c406e7acad6f1047
Deshan200304 <hdganganath003@gmail.com>
2025-11-04 20:58:59 +0530

    check condition is true and if the number is more than 20 get not wired

--- if else.py
+++ if else.py
@@ -9,8 +9,11 @@ number = int(input("Enter a number: "))
 
 if number%2 == 1:
     print("Wired")
-    
+
 elif number%2 == 0 and 2 <= number <= 5:
     print("Non wired")
 elif number%2 == 0 and 6 <= number <= 20:
      print("Weird")
+
+elif number%2 == 0 and number > 20:
+     print("Not Weird")
